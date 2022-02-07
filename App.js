7b19eb3692f809a21da662a3d5550c9203dcb2d4
Nick Maslov <kategorychnost@gmail.com>
2022-02-07 18:06:37 -0500

    Finshed photo gallery RN App

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useReducer, useCallback } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+
+import { getList } from './api/picsum';
+import { actionCreators, initialState, reducer } from './reducers/photos';
+import PhotoGrid from './components/PhotoGrid';
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { photos, nextPage, loading, error } = state;
+  console.log(photos);
+
+  const fetchPhotos = useCallback(async () => {
+    dispatch(actionCreators.loading());
+
+    try {
+      const nextPhotos = await getList(nextPage);
+      dispatch(actionCreators.success(nextPhotos, nextPage));
+    } catch (e) {
+      dispatch(actionCreators.failure());
+    }
+  }, [nextPage]);
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  if (photos.length === 0) {
+    if (loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator animating={true} />
+        </View>
+      );
+    }
+
+    if (error) {
+      return (
+        <View style={styles.container}>
+          <Text>Failed to load photos!</Text>
+        </View>
+      );
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PhotoGrid numColumns={3} photos={photos} onEndReached={fetchPhotos} />
   );
 }
 
